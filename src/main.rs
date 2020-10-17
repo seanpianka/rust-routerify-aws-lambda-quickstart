2224a39d66cf54aa4653adc7282d0b6485623c28
Sean Pianka <pianka@eml.cc>
2020-10-17 19:15:54 -0400

    Update with query param parsing fixes
    Signed-off-by: Sean Pianka <pianka@eml.cc>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use tokio::sync::oneshot;
 use std::convert::Infallible;
 use routerify::prelude::RequestExt;
 use rand::Rng;
+use url;
 
 #[tokio::main]
 async fn main() -> Result<(), AsyncError> {
@@ -14,14 +15,11 @@ async fn main() -> Result<(), AsyncError> {
 }
 
 type AsyncError = Box<dyn std::error::Error + Sync + Send + 'static>;
+const SERVER_ADDR: &str = "127.0.0.1:8080";
 
 async fn start(req: lambda_http::Request) -> Result<impl IntoResponse, AsyncError> {
-    // Generate some random state and build the HTTP router.
-    let router = router(State{ count: rand::thread_rng().gen::<u8>() });
-
-    // Start a internal Routerify server with the above router.
-    let serve = serve(router).await;
-
+    // Store a copy of the query parameters, since AWS Lambda parsed these already.
+    let query_params = req.query_string_parameters();
     // Convert the lambda_http::Request into a hyper::Request.
     let (mut parts, body) = req.into_parts();
     let body = match body {
@@ -29,14 +27,41 @@ async fn start(req: lambda_http::Request) -> Result<impl IntoResponse, AsyncErro
         lambda_http::Body::Text(t) => hyper::Body::from(t.into_bytes()),
         lambda_http::Body::Binary(b) => hyper::Body::from(b),
     };
-    // Prefix the local Routerify's address to the path of the incoming Lambda request.
-    let uri = format!("http://{}{}", serve.addr(), parts.uri.path());
-    parts.uri = hyper::Uri::from_str(uri.as_str()).unwrap();
+    // Prefix the local Routerify server's address to the path of the incoming Lambda request.
+    let mut uri = format!("http://{}{}", SERVER_ADDR, parts.uri.path());
+    // AWS Lambda Rust Runtime will automatically parse the query params *and* remove those
+    // query parameters from the original URI. This is fine if you're writing your logic directly
+    // in the handler function, but for passing-through to a separate router library, we need to
+    // re-url-encode the query parameters and place them back into the URI.
+    if !query_params.is_empty() {
+        uri += "?";
+        // Create a peekable iterator over the query parameters. This is used to add "&" in between
+        // each of the query parameters, but prevents adding an extraneous "&" at the end of the
+        // query parameter string.
+        let mut params = query_params.iter().peekable();
+        while let Some((key, value)) = params.next() {
+            uri += url::form_urlencoded::Serializer::new(String::new())
+                .append_pair(key, value)
+                .finish()
+                .as_str();
+            // If this is not the last parameter, append a "&" for the next parameter...
+            if params.peek().is_some() {
+                uri += "&";
+            }
+        }
+    }
+    parts.uri = match hyper::Uri::from_str(uri.as_str()) {
+        Ok(uri) => uri,
+        Err(e) => panic!(format!("failed to build uri: {:?}", e)),
+    };
     let req = hyper::Request::from_parts(parts, body);
 
+    // Generate some random state and build the HTTP router.
+    let router = router(State{ count: rand::thread_rng().gen::<u8>() });
+    // Start a internal Routerify server with the above router.
+    let serve = serve(router).await;
     // Send the request to the routerify server and return the response.
     let resp = Client::new().request(req).await.unwrap();
-
     // Shutdown the Routerify server.
     serve.shutdown();
 
@@ -84,7 +109,7 @@ pub async fn serve<B, E>(router: Router<B, E>) -> Serve
         <B as hyper::body::HttpBody>::Error: std::error::Error + Send + Sync + 'static,
 {
     let service = RouterService::new(router).unwrap();
-    let server = Server::bind(&([127, 0, 0, 1], 0).into()).serve(service);
+    let server = Server::bind(&SocketAddr::from_str(SERVER_ADDR).unwrap()).serve(service);
     let addr = server.local_addr();
 
     let (tx, rx) = oneshot::channel::<()>();
